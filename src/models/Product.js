ae71b042661910a2a1ec190b0bda63590f6f1d88
VanNgan123 <Nguyenvanngan.0307@gmail.com>
2026-05-13 21:30:05 +0700

    complete product and category module

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-    name : {type: String , required:true},
-    slug: {type: String , required: true},
-    description : {type : String},
-    price : {type : Number, required : true},
-    salePrice :{type: Number},
-    brand :{type: String},
-    categories: [{type : mongoose.Schema.Types.ObjectId,ref : "Category"}],
-    image: [{type:String}],
-    stock:{type : Number , default: 0},
-    specs:{type: Object},
-},{timestamps:true});
+const productSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        slug: { type: String, required: true, unique: true },
+        description: { type: String },
+        price: { type: Number, required: true },
+        salePrice: { type: Number },
+        brand: { type: String },
+        categories: [{ type: mongoose.Schema.Types.ObjectId, ref: "Category" }],
+        image: [{ type: String }],
+        stock: { type: Number, default: 0 },
+        specs: { type: Object },
+    },
+    { timestamps: true }
+);
 
-const Product = mongoose.model("Product",productSchema);
+const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
-
