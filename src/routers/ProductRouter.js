0da040c71cc6d3ba11cfe865de8c6a393ca3076b
VanNgan123 <Nguyenvanngan.0307@gmail.com>
2026-05-19 11:12:58 +0700

    feat: validate route params

--- src/routers/ProductRouter.js
+++ src/routers/ProductRouter.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/ProductController");
-const { authMiddleware, Admin } = require("../middleware/authMiddlware");
-const { validateBody } = require("../middleware/validateMiddleware");
+const { authMiddleware, Admin } = require("../middleware/authMiddleware");
+const { validateBody, validateParams } = require("../middleware/validateMiddleware");
 const { uploadProductImages, handleMulterError } = require("../middleware/uploadMiddleware");
 const {
   createProductSchema,
   updateProductSchema,
 } = require("../validators/productValidator");
+const {
+  mongoIdSchema,
+  mongoIdWithIndexSchema,
+} = require("../validators/commonValidator");
 
 // Public routes
 router.get("/", productController.getAllProduct);
 router.get("/search", productController.searchAndFilterProducts);
-router.get("/:id", productController.getProductById);
+router.get("/:id", validateParams(mongoIdSchema), productController.getProductById);
 
 // Admin routes
 router.post(
@@ -32,6 +36,7 @@ router.put(
   uploadProductImages,
   handleMulterError,
   validateBody(updateProductSchema),
+  validateParams(mongoIdSchema),
   productController.updateProduct
 );
 
@@ -39,6 +44,7 @@ router.delete(
   "/:id",
   authMiddleware,
   Admin,
+  validateParams(mongoIdSchema),
   productController.deleteProduct
 );
 
@@ -46,6 +52,7 @@ router.delete(
   "/:id/image/:index",
   authMiddleware,
   Admin,
+  validateParams(mongoIdWithIndexSchema),
   productController.deleteProductImage
 );
 
